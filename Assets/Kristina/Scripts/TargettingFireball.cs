f8ef842e23ddb1867567946c886af03a55d1bb49
Tina <143371814+Trinketina@users.noreply.github.com>
2024-12-04 12:12:58 -0600

    health updates correctly
    currently no logic for switching phases

--- Assets/Kristina/Scripts/TargettingFireball.cs
+++ Assets/Kristina/Scripts/TargettingFireball.cs
@@ -1,156 +1,151 @@
-using hatsune_miku;
 using System.Collections;
-using System.Collections.Generic;
-using System.Timers;
 using UnityEngine;
-using UnityEngine.Splines;
 
-public class TargettingFireball : MonoBehaviour
+namespace hatsune_miku
 {
-    bool canDodge = true;
-    [SerializeField] Transform initialLocation;
-
-    Transform player;
-    Transform boss;
-    [SerializeField] MeshRenderer dodgeableMesh;
-    [SerializeField] MeshRenderer hitOnlyMesh;
-    SphereCollider scollider;
-
-    bool dodged = false;
-    bool collided = false;
-
-    private void Start()
+    public class TargettingFireball : MonoBehaviour
     {
-        player = FindObjectOfType<PlayerLogic>().transform;
-        boss = FindObjectOfType<BossManager>().transform;
-        scollider = GetComponent<SphereCollider>();
-    }
+        bool canDodge = true;
+        [SerializeField] Transform initialLocation;
 
-    public void StartLockedOnAttack(bool dodgeable)
-    {
-        canDodge = dodgeable;
-        transform.position = initialLocation.position;
-        transform.rotation = initialLocation.rotation;
+        Transform player;
+        Transform boss;
+        [SerializeField] MeshRenderer dodgeableMesh;
+        [SerializeField] MeshRenderer hitOnlyMesh;
+        SphereCollider scollider;
 
-        if (canDodge)
-            dodgeableMesh.enabled = true;
-        else
-            hitOnlyMesh.enabled = true;
-        scollider.enabled = true;
-        dodged = false;
-        collided = false;
-        StartCoroutine(FollowPlayer());
-    }
+        bool dodged = false;
+        bool collided = false;
 
-    private IEnumerator FollowPlayer()
-    {
-        float elapsed = 0;
-
-        float length = 1.5f;
-        while (!collided)
+        private void Start()
         {
-            elapsed += Time.deltaTime;
-            float speed = 10f;
-            /*Vector3 newPos = Vector3.Slerp(initialLocation.position, player.position, elapsed / length);
-            transform.rotation = Quaternion.LookRotation(newPos - transform.position);
-            transform.position = newPos;*/
-
-            transform.LookAt(player);
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-
-            yield return new WaitForEndOfFrame();
+            player = FindObjectOfType<PlayerLogic>().transform;
+            boss = FindObjectOfType<BossManager>().transform;
+            scollider = GetComponent<SphereCollider>();
         }
-        if (dodged)
-        {
-            elapsed = 0;
-            Vector3 crashPos = transform.position;
-            crashPos.y = .2f;
-            Vector3 currPos = transform.position;
-            length = 1f;
-            while (elapsed < length)
-            {
-                Vector3 newPos = Vector3.Slerp(currPos, crashPos, elapsed / length);
-                transform.rotation = Quaternion.LookRotation(newPos - transform.position);
-                transform.position = crashPos;
 
-                elapsed += Time.deltaTime;
+        public void StartLockedOnAttack(bool dodgeable)
+        {
+            canDodge = dodgeable;
+            transform.position = initialLocation.position;
+            transform.rotation = initialLocation.rotation;
+
+            if (canDodge)
+                dodgeableMesh.enabled = true;
+            else
+                hitOnlyMesh.enabled = true;
+            scollider.enabled = true;
+            dodged = false;
+            collided = false;
+            StartCoroutine(FollowPlayer());
+        }
+
+        private IEnumerator FollowPlayer()
+        {
+            transform.LookAt(player);
+
+            while (!collided)
+            {
+                float speed = 10f;
+
+                if (!canDodge)
+                    transform.LookAt(player);
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
                 yield return new WaitForEndOfFrame();
             }
+            if (dodged)
+            {
+                float elapsed = 0;
+                Vector3 crashPos = transform.position;
+                crashPos.y = .2f;
+                Vector3 currPos = transform.position;
+                float length = 1f;
+                while (elapsed < length)
+                {
+                    Vector3 newPos = Vector3.Slerp(currPos, crashPos, elapsed / length);
+                    transform.rotation = Quaternion.LookRotation(newPos - transform.position);
+                    transform.position = crashPos;
+
+                    elapsed += Time.deltaTime;
+
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+            else
+            {
+                ResetFireball();
+            }
         }
-        else
+
+        private IEnumerator FollowBoss()
         {
+            while (!collided)
+            {
+                float speed = 15f;
+
+                transform.LookAt(boss);
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+                yield return new WaitForEndOfFrame();
+            }
+
             ResetFireball();
         }
-    }
 
-    private IEnumerator FollowBoss()
-    {
-        while (!collided)
+        private void OnTriggerEnter(Collider other)
         {
-            float speed = 15f;
+            Damageable damageable = other.GetComponent<Damageable>();
+            if (damageable.GetComponent<PlayerLogic>() != null)
+            {
+                Damage damage = new Damage();
+                damage.amount = 1;
+                damage.direction = transform.forward + Vector3.up;
+                damage.knockbackForce = 2;
 
-            transform.LookAt(boss);
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                if (!damageable.Hit(damage) && canDodge)
+                    dodged = true;
 
-            yield return new WaitForEndOfFrame();
+                collided = true;
+            }
+            else if (damageable.GetComponent<BossManager>() != null)
+            {
+                collided = true;
+
+                gameObject.layer = 9; //return to enemyDamage
+
+                Damage damage = new Damage();
+                damage.amount = 100;
+                damage.direction = Vector3.zero;
+                damage.knockbackForce = 0;
+
+                damageable.Hit(damage);
+            }
+
         }
 
-        ResetFireball();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Damageable damageable = other.GetComponent<Damageable>();
-        if (damageable.GetComponent<PlayerLogic>() != null)
+        private void ResetFireball()
         {
-            Damage damage = new Damage();
-            damage.amount = 1;
-            damage.direction = transform.forward + Vector3.up;
-            damage.knockbackForce = 2;
+            dodgeableMesh.enabled = false;
+            hitOnlyMesh.enabled = false;
+            scollider.enabled = false;
 
-            if (!damageable.Hit(damage) && canDodge)
-                dodged = true;
-
-            collided = true;
+            transform.position = initialLocation.position;
+            transform.rotation = initialLocation.rotation;
         }
-        else if (damageable.GetComponent<BossManager>() != null)
+
+        public void HitBack()
         {
-            collided = true;
+            collided = false;
+            if (!scollider.enabled)
+                return;
 
-            gameObject.layer = 9; //return to enemyDamage
+            if (dodgeableMesh.enabled)
+                transform.position += Vector3.up;
+            gameObject.layer = 8; //swap to PlayerDamage
 
-            Damage damage = new Damage();
-            damage.amount = 100;
-            damage.direction = Vector3.zero;
-            damage.knockbackForce = 0;
-
-            damageable.Hit(damage);
+            StopAllCoroutines();
+            StartCoroutine(FollowBoss());
         }
-        
-    }
-
-    private void ResetFireball()
-    {
-        dodgeableMesh.enabled = false;
-        hitOnlyMesh.enabled = false;
-        scollider.enabled = false;
-
-        transform.position = initialLocation.position;
-        transform.rotation = initialLocation.rotation;
-    }
-
-    public void HitBack()
-    {
-        collided = false;
-        if (!scollider.enabled)
-            return;
-
-        if (dodgeableMesh.enabled)
-            transform.position += Vector3.up;
-        gameObject.layer = 8; //swap to PlayerDamage
-
-        StopAllCoroutines();
-        StartCoroutine(FollowBoss());
     }
 }
